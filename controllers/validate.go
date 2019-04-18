7be0d4e385109ea8523e99b20f6d5be4515ad87e
alvaro <alvaro_enrique_1997@hotmail.com>
2019-04-18 13:46:46 -0500

    se termina la validacion de JWT

---
+++ controllers/validate.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+        "fmt"
+        "github.com/alvaroenriqueds/Dinamo/dinamo-backend/models"
+        "github.com/alvaroenriqueds/edcomments/commons"
+        "github.com/dgrijalva/jwt-go"
+        "github.com/dgrijalva/jwt-go/request"
+        "github.com/labstack/echo"
+        "net/http"
+)
+
+func ValidateToken(c echo.Context) error{
+        token, err := request.ParseFromRequestWithClaims(
+                c.Request(),
+                request.OAuth2Extractor,
+                &models.Claim{},
+                func(token *jwt.Token) (interface{}, error) {
+                    return commons.PublicKey, nil
+                },
+        )
+        if err != nil {
+                switch err.(type) {
+                case *jwt.ValidationError:
+                        vError := err.(*jwt.ValidationError)
+                        switch vError.Errors {
+                        case jwt.ValidationErrorExpired:
+                                fmt.Fprintln(c.Response(), "Su token ha expirado")
+                                return c.NoContent(http.StatusNoContent)
+                        case jwt.ValidationErrorSignatureInvalid:
+                                fmt.Fprintln(c.Response(), "La firma del token no coincide")
+                                return c.NoContent(http.StatusNoContent)
+                        default:
+                                fmt.Fprintln(c.Response(), "Su token no es valido")
+                                return c.NoContent(http.StatusNoContent)
+                        }
+
+                }
+        }
+
+        if token.Valid {
+                //ctx := context.WithValue(c.Request().Context(), "user", token.Claims.(*models.Claim).User)
+                //next(w, r.WithContext(ctx))
+                fmt.Fprintln(c.Response(), "Bienvenido al sistema")
+                return c.String(http.StatusOK, "Si ingreso")
+        }else {
+                return c.String(http.StatusUnauthorized, "Su token no es validp")
+        }
+
+
+}
